d2d35b31740e7b865cd514000852668dfe8bd8a8
matsu.jp <chemical.plek@gmial.com>
2024-04-17 00:10:46 +0900

    edit main.py

--- src/main.py
+++ src/main.py
@@ -1,98 +1,127 @@
-# %%
 import pandas as pd
 import pulp
 import itertools as it
-import math
 
-df = pd.read_csv(
-    "src/data.csv",
-)
 
-name_list = df["name"].to_list()
-hc_dict = dict(zip(df["name"], df["hc"]))
-priority_dict = dict(zip(df["name"], df["priority"].fillna(0)))
-separate_dict = dict(zip(df["name"], df["separate"].fillna(0)))
-inout_list = ["in", "out"]
-group_num = 4
-one_side_group_num_list = range(math.ceil(group_num / 2))
+df_member = pd.read_excel("src/data.xlsx", sheet_name="member")
+df_group = pd.read_excel("src/data.xlsx", sheet_name="group")
+
+member_name_list = df_member["name"].to_list()
+member_hc_dict = dict(zip(df_member["name"], df_member["hc"]))
+member_separate_dict = dict(zip(df_member["name"], df_member["separate"].fillna(0)))
+member_fix_dict = dict(zip(df_member["name"], df_member["fix"].fillna(0)))
+
+group_name_list = df_group["name"].to_list()
+group_num_dict = dict(zip(df_group["name"], df_group["num"]))
+group_priority_dict = dict(zip(df_group["name"], df_group["priority"]))
 
 
 model = pulp.LpProblem(sense=pulp.LpMinimize)
 x = pulp.LpVariable.dicts(
     name="x",
-    indices=it.product(name_list, inout_list, one_side_group_num_list),
+    indices=it.product(member_name_list, group_name_list),
     cat=pulp.LpBinary,
 )
 #
+for member_name, group_name in it.product(member_name_list, group_name_list):
+    if member_fix_dict[member_name] == group_name:
+        model += x[member_name, group_name] == 1
+#
+for member_name in member_name_list:
+    model += (
+        pulp.lpSum(x[member_name, group_name] for group_name in group_name_list) == 1
+    )
+#
+for group_name in group_name_list:
+    model += (
+        pulp.lpSum(x[member_name, group_name] for member_name in member_name_list)
+        <= group_num_dict[group_name]
+    )
+#
 y = pulp.LpVariable.dicts(
     name="y",
-    indices=it.combinations(it.product(inout_list, one_side_group_num_list), r=2),
+    indices=it.product(member_name_list, group_name_list),
     cat=pulp.LpContinuous,
 )
-group_hc = {}
-group_n = {}
-for inout, num in it.product(inout_list, one_side_group_num_list):
-    group_hc[inout, num] = pulp.lpSum(
-        [x[name, inout, num] * hc_dict[name] for name in name_list]
+group_ahc = {}
+for group_name in group_name_list:
+    group_ahc[group_name] = (
+        pulp.lpSum(
+            [x[name, group_name] * member_hc_dict[name] for name in member_name_list]
+        )
+        / group_num_dict[group_name]
     )
-    group_n[inout, num] = pulp.lpSum([x[name, inout, num] for name in name_list])
 
 obj1 = pulp.lpSum(
-    y[a, b]
-    for a, b in it.combinations(it.product(inout_list, one_side_group_num_list), r=2)
+    y[member_name, group_name]
+    for member_name, group_name in it.product(member_name_list, group_name_list)
 )
-for a, b in it.combinations(it.product(inout_list, one_side_group_num_list), r=2):
-    model += y[a, b] >= group_hc[a] / group_n[a] - group_hc[b] / group_n[b]
-    model += y[a, b] >= -(group_hc[a] / group_n[a] - group_hc[b] / group_n[b])
+for member_name, group_name in it.product(member_name_list, group_name_list):
+    model += (
+        y[member_name, group_name]
+        <= member_hc_dict[member_name] * x[member_name, group_name]
+        - group_ahc[group_name]
+    )
+    model += y[member_name, group_name] <= -(
+        member_hc_dict[member_name] * x[member_name, group_name] - group_ahc[group_name]
+    )
 #
 z = pulp.LpVariable.dicts(
     name="z",
-    indices=it.combinations(it.product(inout_list, one_side_group_num_list), r=2),
+    indices=group_name_list,
     cat=pulp.LpContinuous,
 )
-group_num = {}
-for inout, num in it.product(inout_list, one_side_group_num_list):
-    group_num[inout, num] = pulp.lpSum([x[name, inout, num] for name in name_list])
-obj2 = pulp.lpSum(
-    z[a, b]
-    for a, b in it.combinations(it.product(inout_list, one_side_group_num_list), r=2)
+ahc = pulp.lpSum(group_ahc[group_name] for group_name in group_name_list) / len(
+    group_name_list
 )
-for a, b in it.combinations(it.product(inout_list, one_side_group_num_list), r=2):
-    model += z[a, b] >= group_hc[a] - group_hc[b]
-    model += z[a, b] >= -(group_hc[a] - group_hc[b])
-#
-for name in name_list:
-    if priority_dict[name] == 1:
-        obj3 = pulp.lpSum(
-            999 * x[name, inout, num]
-            for inout, num in it.product(inout_list, one_side_group_num_list)
-            if num != 0
-        )
-#
-for inout, num in it.product(inout_list, one_side_group_num_list):
-    obj4 = 999 * (
-        pulp.lpSum(
-            x[name, inout, num] for name in name_list if separate_dict[name] == 1
-        )
-        - 1
-    )
-#
-for name in name_list:
+obj2 = pulp.lpSum(z[group_name] for group_name in group_name_list)
+for group_name in group_name_list:
     model += (
-        pulp.lpSum(
-            x[name, inout, num]
-            for inout, num in it.product(inout_list, one_side_group_num_list)
+        z[group_name]
+        >= pulp.lpSum(
+            member_hc_dict[member_name] * x[member_name, group_name]
+            for member_name in member_name_list
         )
-        == 1
+        - ahc
+    )
+    model += z[group_name] >= -(
+        pulp.lpSum(
+            member_hc_dict[member_name] * x[member_name, group_name]
+            for member_name in member_name_list
+        )
+        - ahc
     )
 #
-for inout, num in it.product(inout_list, one_side_group_num_list):
-    model += (pulp.lpSum(x[name, inout, num] for name in name_list)) <= 4
+# for name in member_name_list:
+#     if member_priority_dict[name] == 1:
+#         obj3 = -pulp.lpSum(
+#             x[name, group_name]
+#             for group_name in group_name_list
+#             if group_priority_dict[group_name] == 1
+#         )
 #
-for inout, num in it.product(inout_list, one_side_group_num_list):
-    model += (pulp.lpSum(x[name, inout, num] for name in name_list)) >= 2
 
-model += obj1 + obj2 + obj3 + obj4
+h = pulp.LpVariable(
+    name="h",
+    cat=pulp.LpContinuous,
+)
+m = pulp.LpVariable(
+    name="l",
+    cat=pulp.LpContinuous,
+)
+sep_point = {}
+for group_name in group_name_list:
+    sep_point[group_name] = pulp.lpSum(
+        x[name, group_name]
+        for name in member_name_list
+        if member_separate_dict[name] == 1
+    )
+obj4 = h - m
+for group_name in group_name_list:
+    model += sep_point[group_name] <= h
+    model += sep_point[group_name] >= m
+
+model += -obj1 + obj2 + 99 * obj4
 # %%
 solver = pulp.FSCIP_CMD(path="C:/Program Files/SCIPOptSuite 9.0.0/bin/fscip.exe")
 status = model.solve(solver=solver)
@@ -101,23 +130,19 @@ print(pulp.LpStatus[status])
 print(model.objective.value())
 # %%
 result = []
-for name in name_list:
-    for inout, num in it.product(inout_list, one_side_group_num_list):
-        if x[name, inout, num].value() == 1:
+for member_name in member_name_list:
+    for group_name in group_name_list:
+        if round(x[member_name, group_name].value()) == 1:
             result.append(
                 [
-                    name,
-                    hc_dict[name],
-                    priority_dict[name],
-                    separate_dict[name],
-                    inout,
-                    num,
+                    member_name,
+                    member_hc_dict[member_name],
+                    member_separate_dict[member_name],
+                    group_name,
                 ]
             )
             break
 
-df = pd.DataFrame(
-    data=result, columns=["name", "hc", "priority", "separate", "in-out", "no"]
-)
-df.to_csv("result.csv", index=False)
-print(df)
+df_member = pd.DataFrame(data=result, columns=["name", "hc", "separate", "group"])
+df_member.to_csv("result_member.csv", index=False)
+print(df_member)
